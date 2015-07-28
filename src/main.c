a93f0ee7a7df28afb6113a7c622c4a495f88d64d
kenneth.schlatter@gmail.com <kenneth.schlatter@gmail.com>
2015-07-28 15:00:58 -0500

    Detected Tap/Twist
    At this point it shows the splash screen (permanently) and detects a "shake"

--- src/main.c
+++ src/main.c
@@ -1,78 +1,62 @@
 #include <pebble.h>
-	
-#define NUM_MENU_SECTIONS 1
-#define DICE_MENU_ITEMS 6
-#define SIDES_MENU_ITEMS 29
 
+//Windows
 static Window *main_window;
-static SimpleMenuLayer *menu_layer;
-static SimpleMenuSection menu_sections[NUM_MENU_SECTIONS];
-static SimpleMenuItem dice_menu_items[DICE_MENU_ITEMS];
-static SimpleMenuItem sides_menu_items[SIDES_MENU_ITEMS];
-unsigned char die1;
-unsigned char die2;
-unsigned char die3;
-unsigned char die4;
-unsigned char die5;
-unsigned char die6;
-unsigned char number_die;
-unsigned char number_sides;
 
+//Text Layers
+
+//Fonts
+
+//Bitmaps
+static BitmapLayer *splash;
+static BitmapLayer *results;
+static BitmapLayer *background_layer;
+static GBitmap *background_bitmap;
+
+//Variables
+unsigned short dice;
+
+//What to do on twist
 static void tap_handler(AccelAxisType axis, int32_t direction) {
-	
-	//Vibrate on shake
-	void vibes_long_pulse(void);
-	
-	//Give the first die a random value
-	die1 = (rand() % 6) + 1;
-	
-	//die 2
-	if (number_die > 1) {
-		die2 = (rand() % 6) + 1;
-		
-		//die 3
-		if (number_die > 2) {
-			die3 = (rand() % 6) + 1;
-			
-			//die 4
-			if (number_die > 3) {
-				die4 = (rand() % 6) + 1;
-				
-				//die 5
-				if (number_die > 4) {
-					die5 = (rand() % 6) + 1;
-					
-					// die 6
-					if (number_die > 5) {
-						die6 = (rand() % 6) + 1;
-					}
-				}
-			}
-			
-		}
-	}
+	APP_LOG(APP_LOG_LEVEL_INFO, "Detected Tap/Twist");
 }
 
+//Make window
 static void main_window_load(Window *window) {
+  // Create GBitmap, then set to created BitmapLayer
+  background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SPLASH);
+  background_layer = bitmap_layer_create(GRect(0, 1, 144, 180));
+  bitmap_layer_set_bitmap(background_layer, background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(background_layer));
 	
-	//"Subscribe" to "Tap" handler
+	//Subscribe to Accelerometer tap service
 	accel_tap_service_subscribe(tap_handler);
 }
 
-static void main_window_unload(Window *window) {
-	
-}
+//static void main_window_unload(Window *window) {}
 
 static void init() {
+	// Create main Window element and assign to pointer
+  main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(main_window, (WindowHandlers) {
+    .load = main_window_load,
+    //.unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+	window_stack_push(main_window, true);
 	
-	//Sets up more random random number
-	srand(time(NULL));
 }
 
 static void deinit() {
-	
+    // Destroy Window
+    window_destroy(main_window);
 }
 
 int main(void) {
-	
+  init();
+  app_event_loop();
+  deinit();
 }
